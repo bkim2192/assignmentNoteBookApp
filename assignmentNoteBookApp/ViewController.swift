fbc68503db2cd347759f97ff1cc8ee2f611a8b16
Brandon Kim <bkim2192@jh-mba158-16.d214.org>
2019-02-26 14:27:57 -0600

    Initial Commit

---
+++ assignmentNoteBookApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignmentNoteBookApp
+//
+//  Created by Brandon Kim on 2/26/19.
+//  Copyright © 2019 Brandon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
