d37135f3433ec78ce41c06a5d150003d89d55404
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-02-26 14:54:48 -0600

    Initial Commit

--- assignmentNoteBookApp/ViewController.swift
+++ assignmentNoteBookApp/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var array:[String] = []
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      tableView.dataSource = self
+        
+        
     }
 
-
+    @IBAction func addItem(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
